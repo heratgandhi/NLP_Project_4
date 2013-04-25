066d3823fa15fb8d78946bd6910cf21c1a6c07eb
Herat <hag59@cornell.edu>
2013-04-25 19:05:48 -0400

    maxofthree

---
+++ src/MaximumThreeModel.java
@@ -0,0 +1,16 @@
+
+public class MaximumThreeModel {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
