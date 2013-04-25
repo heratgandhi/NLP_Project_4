37e21d3236c7c20753995a90b3aa785c53b3b102
Herat <hag59@cornell.edu>
2013-04-25 19:29:22 -0400

    combine 3 models of knn

--- src/MaximumThreeModel.java
+++ src/MaximumThreeModel.java
@@ -1,3 +1,4 @@
+import java.io.*;
 
 public class MaximumThreeModel {
 
@@ -7,6 +8,37 @@ public class MaximumThreeModel {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
+			BufferedReader[] br = new BufferedReader[3];
+			br[0] = new BufferedReader(new FileReader(args[0]));
+			br[1] = new BufferedReader(new FileReader(args[1]));
+			br[2] = new BufferedReader(new FileReader(args[2]));
+			
+			BufferedWriter bw = new BufferedWriter(new FileWriter("FinalOutput"));
+			
+			String line = "",line1,line2;
+			int zv,ov;
+			while((line=br[0].readLine())!= null) {
+				line1 = br[1].readLine();
+				line2 = br[2].readLine();
+				
+				zv = 0;
+				ov = 0;
+				
+				if(Integer.parseInt(line) == 0) zv++;
+				else ov++;
+				if(Integer.parseInt(line1) == 0) zv++;
+				else ov++;
+				if(Integer.parseInt(line2) == 0) zv++;
+				else ov++;
+				
+				if(zv < ov) {
+					bw.write("1\n");
+				} else {
+					bw.write("0\n");
+				}
+			}
+			
+			bw.close();
 			
 		} catch(Exception e) {
 			e.printStackTrace();
